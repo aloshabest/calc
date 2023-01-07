2aabd6ba7fb8d2001859460a04b36c248c8cc5dd
bojenka <stormwww@yandex.ru>
2023-01-07 19:18:17 +0300

    fix

--- calculator.go
+++ calculator.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	var x, y, r1, r2 int
+	var x, y, rim1, rim2 int
 
 	m := map[string]int{
 		"I":    1,
@@ -23,6 +23,29 @@ func main() {
 		"VIII": 8,
 		"IX":   9,
 		"X":    10,
+	}
+
+	m2 := map[int]string{
+		1:  "I",
+		2:  "II",
+		3:  "III",
+		4:  "IV",
+		5:  "V",
+		6:  "VI",
+		7:  "VII",
+		8:  "VIII",
+		9:  "IX",
+		10: "X",
+		11: "XI",
+		12: "XII",
+		13: "XIII",
+		14: "XIV",
+		15: "XV",
+		16: "XVI",
+		17: "XVII",
+		18: "XVIII",
+		19: "XIX",
+		20: "XX",
 	}
 
 	in := bufio.NewReader(os.Stdin)
@@ -38,51 +61,48 @@ func main() {
 		fmt.Println(err1)
 		return
 	}
+	x, rim1 = check(s1[0], m)
 
-	if d, err := strconv.Atoi(s1[0]); err == nil {
-		x = d
-		r1 = 0
-	} else {
-		val, ok := m[s1[0]]
-		if ok {
-			x = val
-			r1 = 1
-		}
-	}
+	y, rim2 = check(s1[2], m)
 
-	if q, err := strconv.Atoi(s1[2]); err == nil {
-		y = q
-		r2 = 0
-	} else {
-		val, ok := m[s1[2]]
-		if ok {
-			y = val
-			r2 = 1
-		}
-	}
-
-	err_rim := ckeck_rim(r1, r2)
+	err_rim := ckeck_rim(rim1, rim2)
 	if err_rim != nil {
 		fmt.Println(err_rim)
 		return
 	}
 
 	if s1[1] == "+" {
-		fmt.Println(x + y)
-	} else if s1[1] == "-" {
-		if r1 == 1 && r2 == 1 {
-			err := ckeck_rim2(x, y)
+		if rim1 == 0 {
+			fmt.Println(x + y)
+		} else {
+			fmt.Println(int_to_rim(x+y, m2))
+		}
 
+	} else if s1[1] == "-" {
+		if rim1 == 0 {
+			fmt.Println(x - y)
+		} else {
+			err := ckeck_rim2(x, y)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
+			fmt.Println(int_to_rim(x-y, m2))
 		}
-		fmt.Println(x - y)
+
 	} else if s1[1] == "*" {
-		fmt.Println(x * y)
+		if rim1 == 0 {
+			fmt.Println(x * y)
+		} else {
+			fmt.Println(int_to_rim(x*y, m2))
+		}
+
 	} else if s1[1] == "/" {
-		fmt.Println(x / y)
+		if rim1 == 0 {
+			fmt.Println(x / y)
+		} else {
+			fmt.Println(int_to_rim(x/y, m2))
+		}
 	}
 
 }
@@ -96,6 +116,36 @@ func filterNewLines(s string) string {
 			return r
 		}
 	}, s)
+}
+
+func int_to_rim(a int, m2 map[int]string) string {
+	var q string
+	val, ok := m2[a]
+	if ok {
+		q = val
+	}
+	return q
+}
+
+func check(s1 string, m map[string]int) (int, int) {
+	var a, b int
+	if d, err := strconv.Atoi(s1); err == nil {
+		err_new := ckeck_ch(d)
+		if err_new != nil {
+			fmt.Println(err_new)
+			return 0, 0
+		}
+		a = d
+		b = 0
+
+	} else {
+		val, ok := m[s1]
+		if ok {
+			a = val
+			b = 1
+		}
+	}
+	return a, b
 }
 
 func ckeck_rim(r1 int, r2 int) error {
@@ -118,6 +168,13 @@ func ckeck_oper(s1 []string) error {
 	}
 	if len(s1) < 3 {
 		return errors.New("Ошибка, так как строка не является математической операцией.")
+	}
+	return nil
+}
+
+func ckeck_ch(n int) error {
+	if n < 1 || n > 10 {
+		return errors.New("Ошибка, значения должны быть от 1 до 10.")
 	}
 	return nil
 }
